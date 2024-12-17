8978f1792ddee148817b5cdf0c95b05be8557634
Clovis <jocailleclovis59@mgmail.com>
2024-12-17 16:55:51 +0100

    add get/post

---
+++ data/moviesData.js
@@ -0,0 +1,17 @@
+export const movies = [
+    {
+        id : 1,
+        title : 'Forest Gump',
+        genre : 'Comedy'
+    },
+    {
+        id : 2,
+        title : 'Spy X Family Code: White',
+        genre : 'Action'
+    },
+    {
+        id : 3,
+        title : 'Solo Leveling',
+        genre : 'Action'
+    },
+]
--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 import express from 'express'
+import 'dotenv/config'
+import {movies} from './data/moviesData.js'
 
 const app = express()
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
 
 app.get('/', (request, response) => {
     response.send("Welcome to my API")
 })
 
-app.listen(3002, () => console.log("Server is running on port 3002"))
+app.get('/movies', (request, response) => {
+    response.json(movies)
+})
+
+app.get('/movie/:id', (req, res) => {
+    let {id} = req.params
+    try{
+        const movieById = movies.find(movie => movie.id == id)
+        if(!movieById){
+            return res.status(400).json({message: "Movie not found"})
+        }
+        return res.json(movieById)
+    }
+    catch(err){
+        return res.status(500).json({message : 'Internal server error'})
+    }
+})
+
+app.post('/movies', (req, res) => {
+    let {title, genre} = req.body
+    const newMovie = {
+        id : movies.lenght + 1,
+        title,
+        genre,
+    }
+    movies.push(newMovie)
+    return res.json(movies)
+})
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
